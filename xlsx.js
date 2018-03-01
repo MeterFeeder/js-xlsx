e68a9a85d612263e13c31498465ceded7921b160
Dan Lopretto <timemachine3030@gmail.com>
2018-03-01 16:15:02 -0500

    Commented out the formula array sections as it caused blank cells

--- xlsx.js
+++ xlsx.js
@@ -11981,10 +11981,10 @@ function write_ws_xml_cell(cell, ref, ws, opts) {
 			o.t = "str"; break;
 	}
 	if(cell.t != oldt) { cell.t = oldt; cell.v = oldv; }
-	if(cell.f) {
-		var ff = cell.F && cell.F.slice(0, ref.length) == ref ? {t:"array", ref:cell.F} : null;
-		v = writextag('f', escapexml(cell.f), ff) + (cell.v != null ? v : "");
-	}
+	// if(cell.f) {
+	// 	var ff = cell.F && cell.F.slice(0, ref.length) == ref ? {t:"array", ref:cell.F} : null;
+	// 	v = writextag('f', escapexml(cell.f), ff) + (cell.v != null ? v : "");
+	// }
 	if(cell.l) ws['!links'].push([ref, cell.l]);
 	if(cell.c) ws['!comments'].push([ref, cell.c]);
 	return writextag('c', v, o);
